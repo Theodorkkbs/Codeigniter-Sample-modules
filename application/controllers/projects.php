d0b55108e7ecd09bf382715d0c2e03375f43f192
Theodorkkbs <45051986+Theodorkkbs@users.noreply.github.com>
2020-12-28 13:56:51 +0200

    Update projects.php

--- application/controllers/projects.php
+++ application/controllers/projects.php
@@ -5,9 +5,9 @@
 
 class Projects extends CI_Controller{
 
-public function_construct(){
+public function __construct(){
 
-parent::_construct();
+parent::__construct();
 
 if(!$this->session->userdata('logged_in')){
 	$this->session->set_flashdata('no_access','Sorry you are not allowed or not logged in');
